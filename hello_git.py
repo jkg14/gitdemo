7a8a68481fdbb4cf3cf9c61e5be120b81812c5cf
Jakub Gajdul <jakub.gajdul@infinityworks.com>
2022-05-05 12:18:37 +0100

    comment on add

--- hello_git.py
+++ hello_git.py
@@ -1,5 +1,5 @@
 # Intro to version control
 print('0. See what changed (git status')
-print('1. Add changed file')
+print('1. Add changed file (prepare for commit)')
 print('2. Make a commit (named transaction)')
 print('3. Push a commit (send to server)')
